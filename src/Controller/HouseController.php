55bade5cdea7f80d1b13f5c5ec5e7960d0b6074c
Thib <thibaud.berdin@groupeberdin.com>
2020-11-13 23:49:37 +0100

    add house controller

---
+++ src/Controller/HouseController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Flex\Unpack\Result;
+
+class HouseController extends AbstractController
+{
+    /**
+     * @Route("/house", name="house")
+     */
+    public function index(): Response
+    {
+        return new Response(<<<EOF
+            <html>
+            <h1> HELLO</h1>
+            </html>
+            EOF
+    );
+    }
+}
