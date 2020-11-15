b9a786ae82004da12dc7a25dfbed9386a4164bb6
Thib <thibaud.berdin@groupeberdin.com>
2020-11-15 17:14:15 +0100

    add fields in house entity and doctrine validator

--- src/Entity/House.php
+++ src/Entity/House.php
@@ -4,9 +4,13 @@ namespace App\Entity;
 
 use App\Repository\HouseRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Exception;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=HouseRepository::class)
+ * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity("name")
  */
 class House
 {
@@ -18,9 +22,78 @@ class House
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
      */
     private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $livingSpace;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $annexSurface;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $roomNumber;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bathroomNumber;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $sellingPriceDf;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $roofing;
+
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $length;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $width;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $height;
+
+
 
     public function getId(): ?int
     {
@@ -38,4 +111,190 @@ class House
 
         return $this;
     }
-}
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getLivingSpace(): ?float
+    {
+        return $this->livingSpace;
+    }
+
+    public function setLivingSpace(float $livingSpace): self
+    {
+        $this->livingSpace = $livingSpace;
+
+        return $this;
+    }
+
+    public function getAnnexSurface(): ?float
+    {
+        return $this->annexSurface;
+    }
+
+    public function setAnnexSurface(?float $annexSurface): self
+    {
+        $this->annexSurface = $annexSurface;
+
+        return $this;
+    }
+
+    public function getRoomNumber(): ?int
+    {
+        return $this->roomNumber;
+    }
+
+    public function setRoomNumber(int $roomNumber): self
+    {
+        $this->roomNumber = $roomNumber;
+
+        return $this;
+    }
+
+    public function getBathroomNumber(): ?int
+    {
+        return $this->bathroomNumber;
+    }
+
+    public function setBathroomNumber(int $bathroomNumber): self
+    {
+        $this->bathroomNumber = $bathroomNumber;
+
+        return $this;
+    }
+
+    public function getSellingPriceDf(): ?float
+    {
+        return $this->sellingPriceDf;
+    }
+
+    public function setSellingPriceDf(float $sellingPriceDf): self
+    {
+        $this->sellingPriceDf = $sellingPriceDf;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @return $this
+     * @throws Exception
+     */
+    public function setCreatedAt(): self
+    {
+        $dtz = new \DateTimeZone('Europe/Paris');
+        $this->createdAt = new \DateTime('now', $dtz);
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @ORM\PreUpdate
+     * @return $this
+     * @throws Exception
+     */
+    public function setUpdatedAt(): self
+    {
+        $dtz = new \DateTimeZone('Europe/Paris');
+        $this->updatedAt = new \DateTime('now',$dtz);
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getRoofing(): ?string
+    {
+        return $this->roofing;
+    }
+
+    public function setRoofing(?string $roofing): self
+    {
+        $this->roofing = $roofing;
+
+        return $this;
+    }
+
+    public function getLength(): ?float
+    {
+        return $this->length;
+    }
+
+    public function setLength(?float $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getWidth(): ?float
+    {
+        return $this->width;
+    }
+
+    public function setWidth(?float $width): self
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    public function getHeight(): ?float
+    {
+        return $this->height;
+    }
+
+    public function setHeight(?float $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+/**
+ * FONCTIONS PERSONNALISSES 
+ */
+
+
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+
+
+    /**
+     * RETOURNE PRIX DE VENTE AVEC TVA
+     */
+    public function getSellingPriceAti(): ?float
+    {
+        return $this->getSellingPriceDf()*1.20;
+    }}
