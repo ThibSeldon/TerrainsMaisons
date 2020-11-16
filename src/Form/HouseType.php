1dba061ef1e24f3d47e0f8cd5ea89163fd2e242f
Thib <thibaud.berdin@groupeberdin.com>
2020-11-16 16:47:08 +0100

    update form house and navebar

--- src/Form/HouseType.php
+++ src/Form/HouseType.php
@@ -4,26 +4,68 @@ namespace App\Form;
 
 use App\Entity\House;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class HouseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('type')
-            ->add('livingSpace')
-            ->add('annexSurface')
-            ->add('roomNumber')
-            ->add('bathroomNumber')
-            ->add('sellingPriceDf')
-            ->add('brand')
-            ->add('roofing')
-            ->add('length')
-            ->add('width')
-            ->add('height')
+            ->add('name',TextType::class, [
+                'label' => 'Nom du modèle'
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                'Plain-Pied' =>'plain-pied',
+                'Etage' => 'etage',
+                'Combles Amémagées' => 'combles_amenagees',
+                ]
+                ])
+            ->add('livingSpace', NumberType::class, [
+                'label' => 'Surface Habitable'
+            ])
+            ->add('annexSurface', NumberType::class, [
+                'label' => 'Surface Annexe',
+            ])
+            ->add('roomNumber', NumberType::class, [
+                'label' => 'Nombre de chambre'
+            ])
+            ->add('bathroomNumber', NumberType::class, [
+                'label' => 'Nombre de Salle de Bains'
+            ])
+            ->add('sellingPriceDf', MoneyType::class, [
+                'label' => 'Prix de Vente HT',
+            ])
+            ->add('brand', ChoiceType::class, [
+                'choices' => [
+                    'Maisons Berdin' => 'maisons_berdin',
+                    'Natilia' => 'natilia',
+                    'Villas Club' => 'villas_club',
+                ]
+            ])
+            ->add('roofing', ChoiceType::class, [
+                'label' => 'Toiture',
+                'choices' => [
+                    'Toiture Traditionnelle' => 'toiture_traditionnelle',
+                    'Toiture BacAcier' => 'toiture bacacier',
+                    'Toiture Terrasse' => 'toiture_terrasse',
+                ]
+            ])
+            ->add('length', NumberType::class, [
+                'label' => 'Longeur'
+            ])
+            ->add('width', NumberType::class, [
+                'label' => 'Largeur'
+            ])
+            ->add('height', NumberType::class, [
+                'label' => 'Hauteur'
+            ])
         ;
     }
 
