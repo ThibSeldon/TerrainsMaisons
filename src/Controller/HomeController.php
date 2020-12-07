bbcbc66187e6cd3b6a0f6cdd134d1fdee001b259
Thib <thibaud.berdin@groupeberdin.com>
2020-12-07 15:10:30 +0100

    add front home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,63 @@
 
 namespace App\Controller;
 
+use App\Entity\Land\Allotment;
+use App\Entity\Land\Plot;
 use App\Repository\HouseRepository;
+use App\Repository\Land\AllotmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HomeController
+ * @package App\Controller
+ * @Route("/home")
+ */
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
+     * @param HouseRepository $houseRepository
+     * @param AllotmentRepository $allotmentRepository
+     * @return Response
      */
-    public function index(HouseRepository $houseRepository): Response
+    public function index(HouseRepository $houseRepository, AllotmentRepository $allotmentRepository): Response
     {
         $houses = $houseRepository->loadHouseBerdin();
+        $allotments = $allotmentRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'houses' => $houses,
+            'allotments' => $allotments,
+        ]);
+    }
+
+    /**
+     * @Route("home/allotment/plot/{id}", name="all_plot_show")
+     * @param Plot $plot
+     * @param HouseRepository $houseRepository
+     * @return Response
+     */
+    public function lot(Plot $plot, HouseRepository $houseRepository): Response
+    {
+        $houses = $houseRepository->findHousesPlotCompatible($plot->getFacadeWidth());
+
+        return $this->render('home/plot_houses.html.twig', [
+            'houses' => $houses,
+            'plot' => $plot
+        ]);
+    }
+
+    /**
+     * @Route ("/home/{id}", name="all_allotment_show")
+     * @param Allotment $allotment
+     * @return Response
+     */
+    public function allotment(Allotment $allotment): Response
+    {
+        return $this->render('home/allotments.html.twig', [
+            'allotment' => $allotment,
         ]);
     }
 }
